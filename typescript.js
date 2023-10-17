7bcf160db3ab04fb469f35d1d25d338308cdca0c
CTC CCP 6 <113914772+8i-ght@users.noreply.github.com>
2023-10-17 13:13:11 -0500

    fix remote repo

---
+++ typescript.js
@@ -0,0 +1,6 @@
+function add(a, b) {
+    return a + b;
+}
+var results = add(2, 5);
+console.log(results);
+// hello
---
+++ typescript.ts
@@ -0,0 +1,9 @@
+function add(a: number, b: number) {
+    return a + b;
+}
+
+const results = add(2, 5);
+
+console.log(results); 
+
+// hello
